10f43703ff68e21e18437e7ca698126c4548d2d6
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-27 16:38:06 +0100

    coder of the month

---
+++ README.md
@@ -0,0 +1,45 @@
+# jspsych-audlexdec-vp
+Template auditory+visual [lexical decision](https://en.wikipedia.org/wiki/Lexical_decision_task) experiment with _masked_ _visual_ prime.
+
+# Task Description
+
+## Short description
+
+
+## Longer description
+
+
+# Getting started (the easy way, working internet connection required)
+For now, the easiest way to test these templates, is:
+
+1. Download this repository by clicking the green code button above and Download zip.
+2. Unzip the jspsych-vislexdec-vp-main.zip at a location of your choosing.
+3. Inside the folder is a file called index.html, double click it in order to open it
+   in a browser.
+
+# Getting started (the harder way, local and/or custom setup)
+
+You need this github repository and to download the jsPsych library version 6.1
+complete the following steps. When downloading and extracting folders please
+keep in mind that once working on the server, filenames are case sensitive and
+"jsPsych.js" and "jspsych.js" are two distinct filenames. It might work on your
+machine, but it might not work on the server hosting your experiment.
+
+You will need to adapt your own directory structure/naming convention in the 
+top ```<script>``` tags as defined in index.html, if you want your own local jsPsych setup.
+
+1. Download this repository by clicking the green code button above and Download zip.
+2. Unzip the jspsych-vislexdec-vp.zip at a location of your choosing.
+3. Download jsPsych-6.1.0 (-6.1.0 is the version) from the jsPsych releases website
+   https://github.com/jspsych/jsPsych/releases. You might need to scroll down a little.
+4. Extract the jsPsych folder into the jspsych-vislexdec-vp folder you extracted earlier.
+5. Inside the folder is a file called index.html, double click it in order to open it
+   in a browser.
+   
+
+
+
+
+
+  
+
---
+++ stimuli.js
@@ -0,0 +1,144 @@
+// Item types
+const NON_WORD = "NON_WORD";
+const RELATED = "RELATED";
+const UNRELATED = "UNRELATED"
+
+const GROUPS = [
+    "group1"
+    // "group2",
+    // "group3"
+];
+
+const PRACTICE_ITEMS = [
+    {
+        id: 1, 
+        item_type: NON_WORD, 
+        word: "palve", 
+        wordfn: "./sounds/palve.wav",
+        prime: "onion",
+        pmask: "#####" //as many monospaced fonts as needed to mask prime
+    },
+    {
+        id: 2, 
+        item_type: UNRELATED, 
+        word: "hot", 
+        wordfn: "./sounds/hot.wav",
+        prime: "stapler",
+        pmask: "######"
+    }
+];
+
+const LIST_GROUP1 = [
+    {
+        id: 1, 
+        item_type: NON_WORD, 
+        word: "slirque", 
+        wordfn: "./sounds/slirque.wav",
+        prime: "eyes",
+        pmask: "####"
+    },
+    {
+        id: 2, 
+        item_type: NON_WORD, 
+        word: "crawse", 
+        wordfn: "./sounds/crawse.wav",
+        prime: "piano",
+        pmask: "#####"
+    },
+    {
+        id: 3, 
+        item_type: NON_WORD, 
+        word: "thwurp", 
+        wordfn: "./sounds/thwurp.wav",
+        prime: "rabbit",
+        pmask: "######"
+    },
+    {
+        id: 4, 
+        item_type: NON_WORD, 
+        word: "clem", 
+        wordfn: "./sounds/clem.wav",
+        prime: "flower",
+        pmask: "######"
+    }, 
+    {
+        id: 5, 
+        item_type: RELATED, 
+        word: "white", 
+        wordfn: "./sounds/white.wav",
+        prime: "snow",
+        pmask: "####"
+    },
+    {
+        id: 6, 
+        item_type: RELATED, 
+        word: "travel", 
+        wordfn: "./sounds/travel.wav",
+        prime: "suitcase",
+        pmask: "########"
+    },
+    {
+        id: 7, 
+        item_type: UNRELATED, 
+        word: "letter", 
+        wordfn: "./sounds/letter.wav",
+        prime: "garden",
+        pmask: "######"
+    },
+    {
+        id: 8, 
+        item_type: UNRELATED, 
+        word: "clown", 
+        wordfn: "./sounds/clown.wav",
+        prime: "forest",
+        pmask: "######"
+    }
+];
+
+
+// Add a second list of stimuli when required.
+// const LIST_GROUP2 = [
+// ...
+// ]
+
+const TEST_ITEMS = [
+    {group_name: GROUPS[0], table: LIST_GROUP1}
+    // Add the second group here, put a comma on the end of the line above here.
+    //{group_name: GROUPS[1], table: LIST_GROUP2}
+];
+
+/**
+ * Get the list of practice items
+ *
+ * Returns an object with a group and a table, the group will always indicate
+ * "practice" since it are the practice items
+ *
+ * @returns {object} object with group and table fields
+ */
+function getPracticeItems() {
+    return {group_name : "practice", table : PRACTICE_ITEMS};
+}
+
+/**
+ * This function will pick a random group from the TEST_ITEMS array.
+ *
+ * Returns an object with a group and a table, the group will always indicate
+ * which list has been chosen for the participant.
+ *
+ * @returns {object} object with group and table fields
+ */
+function pickRandomGroup() {
+    let range = function (n) {
+        let empty_array = [];
+        let i;
+        for (i = 0; i < n; i++) {
+            empty_array.push(i);
+        }
+        return empty_array;
+    }
+    let num_groups = TEST_ITEMS.length;
+    var shuffled_range = jsPsych.randomization.repeat(range(num_groups), 1)
+    var retgroup = TEST_ITEMS[shuffled_range[0]];
+    return retgroup
+}
+
