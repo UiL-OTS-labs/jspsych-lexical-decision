58adc743cb420b98ef9db8c98a89a01a6e4a38c0
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-11 12:48:34 +0100

    removed commented out stuff

--- instructions.js
+++ instructions.js
@@ -5,43 +5,6 @@
  * utf8 text encoding.
  */
 
-
-// const PRE_PRACTICE_INSTRUCTION =
-//     "<strong>Dear participant,</strong><BR><BR>"        +
-
-//     "Thank you for participating in this experiment."   +
-//     "<BR><BR>"                                          +
-
-//     "You are now going to see and hear words, one "     +
-//     "after the other. The words come in pairs, first "  +
-//     "a written word, then a spoken word. Pay attention "+
-//     "to both the written word <i>and</i> the word you " +
-//     "<i>hear</i>.<BR><BR>"                              +
-
-//     "Your task is to indicate as "                      +
-//     "quickly as possible whether you think "            +
-//     "<i>both words are existing words or not</i>. "     +
-//     "Try to make no mistakes.<BR><BR>"                  +
-
-//     "After clicking OK, please put your "               +
-//     "<i>index</i> fingers on the two "                  +
-//     "keys you've just set. Try keep them in those "     +
-//     "positions during the <i>entire experiment</i>!<BR>"+
-
-//     "<ul>"                                              +
-//     "<li>Hit the <kbd>%correct_key%</kbd> key for "     +
-//     "<strong>Yes</strong>. "                            +
-//     "<sub><i>(Use this 'Yes' key to proceed throughout "+
-//     "the experiment)</i>.</sub></li>"                   +
-//     "<li>Hit the <kbd>%incorrect_key%</kbd> key for "   +
-//     "<strong>No</strong>.</li>"                         +
-//     "</ul>"                                             +
-
-//     "First, you can practice.<BR><BR>"                  +
-//     "<i>Click OK to start practicing.</i>"
-//     ;
-
-
 const PRE_PRACTICE_INSTRUCTION =
     "<strong>Dear participant,</strong><BR><BR>"        +
     "Thank you for participating in this experiment."   +
