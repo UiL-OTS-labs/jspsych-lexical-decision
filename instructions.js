ac2d2fabcd8afd84d73f1d33cc19c1c364f524c1
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-31 16:17:33 +0100

    big restyle

--- instructions.js
+++ instructions.js
@@ -5,37 +5,50 @@
  * utf8 text encoding.
  */
 
+ // GENERIC TESTS/CHECKS
+const GENERIC_CHECK = `
+    <h3>In order to participate, please make sure that you:</h3>
+    <BR>
+    <ul>
+    <li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li>
+    <li>Have a <b>real keyboard</b> working.</li><li>Have a <b>mouse</b> and/or 
+    <b>trackpad</b> that works.</li>
+    <li>Have your browser's audio enabled and your volume on.</li>
+    </ul>
+    <h3>Please <i>maximize</i> your browser window before you continue!</h3>
+    <BR>
+    <p>Click below if you are ready to proceed</p>
+    `;
+
 const PRE_PRACTICE_INSTRUCTION =
-    "<strong>Dear participant,</strong><BR><BR>"        +
+    "<strong>Dear participant,</strong><br><br>"        +
     "Thank you for participating in this experiment."   +
-    "<BR><BR>"                                          +
+    "<br><br>"                                          +
 
     "You are now going to see and hear words, one "     +
     "after the other. The words come in pairs, first "  +
     "a written word, then a spoken word. "              +
     "Pay attention to both the written word "           +
     "<i>and</i> the word you "                          +
-    "<i>hear</i>.<BR><BR>"                              +
+    "<i>hear</i>.<br><br>"                              +
 
     "Your task is to indicate as "                      +
     "quickly as possible whether you think "            +
     "<i>both words are existing words or not</i>. "     +
-    "Try to make no mistakes.<BR><BR>"                  +
+    "Try to make no mistakes.<br><br>"                  +
 
     "After clicking OK, please put your "               +
     "<i>index</i> fingers on the two "                  +
     "keys you've just set. Try keep them in those "     +
-    "positions during the <i>entire experiment</i>!<BR>"+
+    "positions during the <i>entire experiment</i>!<br>"+
 
     "<ul>"                                              +
     "<li>Hit the <kbd>%correct_key%</kbd> key for "     +
-    "<strong>Yes</strong>. "                            +
-    "<sub><i>(Use this 'Yes' key to proceed throughout "+
-    "the experiment)</i>.</sub></li>"                   +
+    "<strong>Yes</strong>.</li>"                        +
     "<li>Hit the <kbd>%incorrect_key%</kbd> key for "   +
     "<strong>No</strong>.</li>"                         +
     "</ul>"                                             +
-    "First, you can practice.<BR><BR>"                  +
+    "First, you can practice.<br><br>"                  +
     "<i>Click OK to start practicing.</i>"
 ;
 
@@ -50,3 +63,13 @@ const PRE_TEST_INSTRUCTION =
     "End of the practice part.<BR><BR>"                 +
     "Click OK to continue with the real task."
     ;
+
+const DEBRIEF_MESSAGE = `
+    <h1>End of the experiment</h1>
+    <br>
+    <br>
+    <h2>Thank you for participating!</h2>
+    `;
+
+const DEBRIEF_MESSAGE_DURATION = 3000;
+
--- stimuli.js
+++ stimuli.js
@@ -1,41 +1,53 @@
+////////////////
+// STIMULI
+///////////////
+
 // Item types
 const NON_WORD = "NON_WORD";
 const RELATED = "RELATED";
-const UNRELATED = "UNRELATED"
+const UNRELATED = "UNRELATED";
+const PRACTICE = "PRACTICE";
 
-const GROUPS = [
-    "group1"
-    // "group2",
-    // "group3"
-];
+// name for lists, in this case one list, one item in the list
+const LISTS = ["my_one_and_only_list"];
+
+// In case of more complex design, the above could be, for example:
+
+// const LISTS = [
+//     "my_first_list",
+//     "my_second_list"
+// ];
 
 const PRACTICE_ITEMS = [
     {
         id: 1, 
-        item_type: NON_WORD, 
+        item_type: PRACTICE, 
         word: "palve", 
         wordfn: "./sounds/palve.wav",
         prime: "onion",
-        pmask: "#####" //as many monospaced fonts as needed to mask prime
+        pmask: "#####",
+        correct: 0
     },
     {
         id: 2, 
-        item_type: UNRELATED, 
+        item_type: PRACTICE, 
         word: "hot", 
         wordfn: "./sounds/hot.wav",
         prime: "stapler",
-        pmask: "#######"
+        pmask: "#######",
+        correct: 1
     }
 ];
 
-const LIST_GROUP1 = [
+const LIST_1 = [
     {
         id: 1, 
         item_type: NON_WORD, 
         word: "slirque", 
         wordfn: "./sounds/slirque.wav",
         prime: "eyes",
-        pmask: "####"
+        pmask: "####",
+        correct: 0
     },
     {
         id: 2, 
@@ -43,7 +55,8 @@ const LIST_GROUP1 = [
         word: "crawse", 
         wordfn: "./sounds/crawse.wav",
         prime: "piano",
-        pmask: "#####"
+        pmask: "#####",
+        correct: 0
     },
     {
         id: 3, 
@@ -51,7 +64,8 @@ const LIST_GROUP1 = [
         word: "thwurp", 
         wordfn: "./sounds/thwurp.wav",
         prime: "rabbit",
-        pmask: "######"
+        pmask: "######",
+        correct: 0
     },
     {
         id: 4, 
@@ -59,7 +73,8 @@ const LIST_GROUP1 = [
         word: "clem", 
         wordfn: "./sounds/clem.wav",
         prime: "flower",
-        pmask: "######"
+        pmask: "######",
+        correct: 0
     }, 
     {
         id: 5, 
@@ -67,7 +82,8 @@ const LIST_GROUP1 = [
         word: "white", 
         wordfn: "./sounds/white.wav",
         prime: "snow",
-        pmask: "####"
+        pmask: "####",
+        correct: 1
     },
     {
         id: 6, 
@@ -75,7 +91,8 @@ const LIST_GROUP1 = [
         word: "travel", 
         wordfn: "./sounds/travel.wav",
         prime: "suitcase",
-        pmask: "########"
+        pmask: "########",
+        correct: 1
     },
     {
         id: 7, 
@@ -83,7 +100,8 @@ const LIST_GROUP1 = [
         word: "letter", 
         wordfn: "./sounds/letter.wav",
         prime: "garden",
-        pmask: "######"
+        pmask: "######",
+        correct: 1
     },
     {
         id: 8, 
@@ -91,43 +109,44 @@ const LIST_GROUP1 = [
         word: "clown", 
         wordfn: "./sounds/clown.wav",
         prime: "forest",
-        pmask: "######"
+        pmask: "######",
+        correct: 1
     }
 ];
 
 
 // Add a second list of stimuli when required.
-// const LIST_GROUP2 = [
+// const LIST_2 = [
 // ...
 // ]
 
 const TEST_ITEMS = [
-    {group_name: GROUPS[0], table: LIST_GROUP1}
-    // Add the second group here, put a comma on the end of the line above here.
-    //{group_name: GROUPS[1], table: LIST_GROUP2}
+    {list_name: LISTS[0], table: LIST_1}
+    // Add the second list here, put a comma on the end of the line above here.
+    //{group_name: LISTS[1], table: LIST_2}
 ];
 
 /**
  * Get the list of practice items
  *
- * Returns an object with a group and a table, the group will always indicate
+ * Returns an object with a group and a table, the list will always indicate
  * "practice" since it are the practice items
  *
  * @returns {object} object with group and table fields
  */
 function getPracticeItems() {
-    return {group_name : "practice", table : PRACTICE_ITEMS};
+    return {list_name : "practice", table : PRACTICE_ITEMS};
 }
 
 /**
- * This function will pick a random group from the TEST_ITEMS array.
+ * This function will pick a random list from the TEST_ITEMS array.
  *
- * Returns an object with a group and a table, the group will always indicate
+ * Returns an object with a list and a table, the list will always indicate
  * which list has been chosen for the participant.
  *
- * @returns {object} object with group and table fields
+ * @returns {object} object with list and table fields
  */
-function pickRandomGroup() {
+function pickRandomList() {
     let range = function (n) {
         let empty_array = [];
         let i;
@@ -136,9 +155,9 @@ function pickRandomGroup() {
         }
         return empty_array;
     }
-    let num_groups = TEST_ITEMS.length;
-    var shuffled_range = jsPsych.randomization.repeat(range(num_groups), 1)
-    var retgroup = TEST_ITEMS[shuffled_range[0]];
-    return retgroup
+    let num_lists = TEST_ITEMS.length;
+    var shuffled_range = jsPsych.randomization.repeat(range(num_lists), 1)
+    var retlist = TEST_ITEMS[shuffled_range[0]];
+    return retlist
 }
 
